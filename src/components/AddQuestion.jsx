09dcdf91f68e66b913b5bfa0df5a2044a4fbb0cc
sauhardbhushan <sauhard@hotmail.co.uk>
2020-03-18 22:03:53 +0000

    add firebase connection and views

---
+++ src/components/AddQuestion.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddQuestion = props => {
+    return (
+        <ion-content>
+            <ion-input>
+
+            </ion-input>
+        </ion-content>
+    )
+}
+
+export default AddQuestion;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,49 @@
 import React from 'react'
-import { FirebaseContext } from '../App'
 
 class Question extends React.Component {
 
+    constructor() {
+        super()
+        this.handleClick = this.handleClick.bind(this)
+    }
 
+    handleClick(e) {
+        const userAnswer = e.currentTarget.innerText;
+        const isUserAnswerCorrect = this.props.options[userAnswer]
+
+        this.props.getNextQuestion(isUserAnswerCorrect);
+    }
+
+    renderAnswer(option, key, isCorrect) {
+        return (
+            <ion-row key={key.toString()} class="ion-justify-content-center">
+                <ion-col size="8">
+                    <ion-button class="options" expand="block" shape="round" size="large" is-correct={isCorrect.toString()} onClick={this.handleClick}> {option} </ion-button>
+                </ion-col>
+            </ion-row>
+        )
+    }
 
     render() {
+
         return (
             <div>
-                <FirebaseContext.Consumer>
-                    {
-                        (value) => {
-                            return (<ion-card>
-                                <ion-card-header>Question</ion-card-header>
-                            </ion-card>)
-                        }
-                    }
-                </FirebaseContext.Consumer>
+                <ion-grid class="ion-padding">
+                    <ion-row class="ion-justify-content-center">
+                        <ion-col>
+                            <h1 className="c-question">
+                                {this.props.question}
+                            </h1>
+                            
+                        </ion-col>
+                    </ion-row>
 
+                    { Object.keys(this.props.options).map((option, i) => (
+                        this.renderAnswer(option, i, Object.values(this.props.options)[i])
+                        ))
+                    }
+
+                </ion-grid>
             </div>
         )
     }
