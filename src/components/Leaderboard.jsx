219fa14a4d547c6bc1faacc9cddf11f42c3e2d70
sauhardbhushan <sauhard@hotmail.co.uk>
2020-03-20 23:10:06 +0000

    modify leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../css/App.css'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Home from './Home'
 
 class Leaderboard extends React.Component {
 
     constructor() {
         super();
         this.state = {
+            isLoading: true,
             leaderboard: {}
         }
     }
@@ -13,49 +16,51 @@ class Leaderboard extends React.Component {
     getLeaderboard() {
         this.props.dbRef.once('value', snapshot => {
             const val = snapshot.val()
-            this.setState({ leaderboard: val.leaderboard })
+            this.setState({ isLoading: false, leaderboard: val.leaderboard })
         })
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.getLeaderboard()
     }
 
-
     render() {
 
-        console.log(this.props)
-
         return (
-            <div class="ion-padding">
-                <ion-row class="ion-align-items-center">
-                    <ion-col>
-                        <h1>Leaderboard</h1>
-                    </ion-col>
-                </ion-row>
+            <>
+                <Router>
+                    <Route path="/" exact component={Home}></Route>
+                    <Link to="/"><ion-button>Home</ion-button></Link>
+                </Router>
+                <div className="ion-padding">
+                    <ion-row >
+                        <ion-col class="c-question">
+                            <h1 className="u-text-large">Leaderboard</h1>
 
-                <ion-list>
-                    {Object.entries(this.state.leaderboard).map((key) => {
-                        const [name, score] = key
-                        return (
-                            <ion-item>
-                                <ion-row class="ion-justify-content-around">
-                                    <ion-col size="">
-                                        <ion-label>1.</ion-label>
-                                    </ion-col>
-                                    <ion-col size="">
-                                        <ion-label>{name}</ion-label>
-                                    </ion-col>
-                                    <ion-col size="" class="ion-align-self-end">
+                            <h3 className="">See where you rank amongst the world's top players</h3>
+                        </ion-col>
+                    </ion-row>
+                </div>
+
+                <div className="ion-padding c-leaderboard">
+                    {this.state.isLoading ? <ion-spinner /> :
+                        <ion-list >
+                            {Object.entries(this.state.leaderboard).map((key) => {
+                                const [name, score] = key
+                                return (
+                                    <ion-item key={name} >
+                                        <ion-label>1. </ion-label>
+                                        <ion-label>{name} </ion-label>
                                         <ion-label>{score}</ion-label>
-                                    </ion-col>
-                                </ion-row>
-                            </ion-item>
-                        )
-                    })
+                                    </ion-item>
+                                )
+                            })
+                            }
+                        </ion-list>
                     }
-                </ion-list>
-            </div>
+                </div>
+
+            </>
         )
     }
 }
